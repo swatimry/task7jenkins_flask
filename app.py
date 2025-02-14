709b4327bebb7f9f8fdf3cd32a28ee75ad9352c2
Swati <115008404+swatimry@users.noreply.github.com>
2025-02-14 11:27:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "Hello Swati,This is a  Flask App!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=2222, debug=True)
